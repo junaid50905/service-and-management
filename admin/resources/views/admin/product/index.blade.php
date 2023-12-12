a0610a88c764d42b35249731390fa2931410c937
emonsingha209 <emonsingha209@gmail.com>
2023-12-12 15:51:22 +0600

    design updated

--- admin/resources/views/admin/product/index.blade.php
+++ admin/resources/views/admin/product/index.blade.php
@@ -1,20 +1,19 @@
 @extends('admin.layouts.master')
 
 @section('title')
-    Products
+Products
 @endsection
 
 @section('main-panel')
-    <div class="row">
-        <div class="col-md-12 grid-margin">
-            @if (count($products) < 1)
-            <h4 class="text-danger">No product found</h4>
+<div class="row">
+    <div class="col-md-12 grid-margin">
+        @if (count($products) < 1) <h4 class="text-danger">No product found</h4>
             @else
             <div class="card">
                 <div class="card-body">
                     <h4 class="card-title">Products</h4>
                     <div class="table-responsive">
-                        <table id="example" class="table table-striped" style="width:100%">
+                        <table id="example" class="table table-hover">
                             <thead>
                                 <tr>
                                     <th>#</th>
@@ -28,23 +27,23 @@
                             </thead>
                             <tbody>
                                 @foreach ($products as $product)
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>
-                                            @php
-                                                $wrapped_name = wordwrap($product->name, 40, "\n", true);
-                                            @endphp
-                                            {!! nl2br(e($wrapped_name)) !!}
-                                        </td>
-                                        <td>{{ $product->model }}</td>
-                                        <td>{{ $product->price }}</td>
-                                        <td>{{ DB::table('categories')->where('id', $product->category_id)->first()->name }}</td>
-                                        <td>{{ DB::table('subcategories')->where('id', $product->subcategory_id)->first()->name }}</td>
-                                        <td>
-                                            <a href="{{ route('product.edit', $product->id) }}" class="btn btn-warning me- p-2"><i class="fa-regular fa-pen-to-square"></i></a>
-                                            <a href="{{ route('product.delete', $product->id) }}" onclick="return confirm('are you sure?')" class="btn btn-danger p-2"><i class="fa-solid fa-trash"></i></a>
-                                        </td>
-                                    </tr>
+                                <tr>
+                                    <td>{{ $loop->iteration }}</td>
+                                    <td>
+                                        @php
+                                        $wrapped_name = wordwrap($product->name, 40, "\n", true);
+                                        @endphp
+                                        {!! nl2br(e($wrapped_name)) !!}
+                                    </td>
+                                    <td>{{ $product->model }}</td>
+                                    <td>{{ $product->price }}</td>
+                                    <td>{{ DB::table('categories')->where('id', $product->category_id)->first()->name }}</td>
+                                    <td>{{ DB::table('subcategories')->where('id', $product->subcategory_id)->first()->name }}</td>
+                                    <td>
+                                        <a href="{{ route('product.edit', $product->id) }}" class="p-2 btn btn-warning me-"><i class="fa-regular fa-pen-to-square"></i></a>
+                                        <a href="{{ route('product.delete', $product->id) }}" onclick="return confirm('are you sure?')" class="p-2 btn btn-danger"><i class="fa-solid fa-trash"></i></a>
+                                    </td>
+                                </tr>
                                 @endforeach
                             </tbody>
                             <tfoot>
@@ -64,11 +63,8 @@
             </div>
             @endif
 
-        </div>
     </div>
-
-
-
+</div>
 
 
 
