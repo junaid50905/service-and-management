22cd756b9f7850fc910614b1cb1faee5fd26e693
junaid hossain <junaidhossain50905@gmail.com>
2024-02-21 12:11:00 +0600

    feature: report generated

--- admin/app/Http/Controllers/Admin/DashboardController.php
+++ admin/app/Http/Controllers/Admin/DashboardController.php
@@ -23,7 +23,64 @@ class DashboardController extends Controller
         $totalNumberOfTodaysWorkingTask = Inspection::where('start_date', Carbon::today())->where('inspection', 'start')->get()->count();
         $allTodaysAppiontments = Appiontment::where('appiontment_taken_date', Carbon::today())->get();
 
-        return view('admin.dashboard', compact('totalProducts', 'totalSalesProducts', 'recentsAppiontments', 'allTodaysWorkingTasks', 'totalNumberOfTodaysWorkingTask', 'allTodaysAppiontments'));
+
+        ////////////////// report ////////////////
+        $year_month = Appiontment::selectRaw('DATE_FORMAT(created_at, "%Y-%m") AS formatted_date')
+            ->distinct()
+            ->get()
+            ->pluck('formatted_date');
+
+        $formatted_dates = Appiontment::selectRaw('DATE_FORMAT(created_at, "%Y-%m") AS formatted_date')
+        ->distinct()
+        ->get()
+        ->pluck('formatted_date');
+
+        // appiontments ///////////
+        $appiontments = [];
+        foreach ($formatted_dates as $formatted_date) {
+            // Count the number of rows for each formatted date
+            $count = Appiontment::whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [$formatted_date])
+            ->count();
+            $appiontments[] = $count;
+        }
+
+        // completed rows //////////
+        $completedTasks = [];
+        foreach ($formatted_dates as $formatted_date) {
+            $completedTask = Appiontment::whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [$formatted_date])
+            ->where('status', 'complete')
+            ->count();
+
+            // Store the count in the array
+            $completedTasks[] = $completedTask;
+        }
+
+        // late rows //////////
+        $lateTasks = [];
+        foreach ($formatted_dates as $formatted_date) {
+            $lateTask = Appiontment::whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [$formatted_date])
+                ->where('status', 'late')
+                ->count();
+
+            $lateTasks[] = $lateTask;
+        }
+
+        // working rows //////////
+        $workingTasks = [];
+        foreach ($formatted_dates as $formatted_date) {
+            $workingTask = Appiontment::whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [$formatted_date])
+                ->where('status', 'working')
+                ->count();
+
+            $workingTasks[] = $workingTask;
+        }
+
+
+
+
+
+        return view('admin.dashboard', compact('totalProducts', 'totalSalesProducts', 'recentsAppiontments', 'allTodaysWorkingTasks', 'totalNumberOfTodaysWorkingTask', 'allTodaysAppiontments', 'year_month', 'appiontments', 'completedTasks', 'lateTasks', 'workingTasks'));
+
     }
     // allTodaysTasks
     public function allTodaysWorkingTasks()
--- admin/resources/views/admin/layouts/includes/scripts.blade.php
+++ admin/resources/views/admin/layouts/includes/scripts.blade.php
@@ -1,3 +1,6 @@
+
+
+
 <script src="{{ asset('ui/admin') }}/assets/vendors/js/vendor.bundle.base.js"></script>
 <!-- endinject -->
 <!-- Plugin js for this page -->
@@ -36,8 +39,12 @@
 <script src='https://cdn.datatables.net/responsive/2.1.0/js/dataTables.responsive.min.js'></script>
 <script src='https://cdn.datatables.net/1.13.5/js/jquery.dataTables.min.js'></script>
 <script src='https://cdn.datatables.net/1.13.5/js/dataTables.bootstrap5.min.js'></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js" integrity="sha512-G9dfCJFG3y/Xq8mbbqb5i3FQNVaGl0Fkkw+VPVT3L00gA4k7hyjSGNpAxykwgDw1cfJFlj5tO3XePa+ezjDQyQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js" integrity="sha512-yUpN8Ms0SReogyyMOe6drcsPi9Km1bZr2MmBQSPK5FAeEd6sjFXzxyUieQ+tTPOo0DBhI+3NHGQwlfpog5Jdfg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"
+    integrity="sha512-G9dfCJFG3y/Xq8mbbqb5i3FQNVaGl0Fkkw+VPVT3L00gA4k7hyjSGNpAxykwgDw1cfJFlj5tO3XePa+ezjDQyQ=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"
+    integrity="sha512-yUpN8Ms0SReogyyMOe6drcsPi9Km1bZr2MmBQSPK5FAeEd6sjFXzxyUieQ+tTPOo0DBhI+3NHGQwlfpog5Jdfg=="
+    crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 
 <script>
@@ -69,17 +76,14 @@
 </script>
 
 <script>
-
-      document.addEventListener('DOMContentLoaded', function() {
+    document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
         var calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth'
+            initialView: 'dayGridMonth'
         });
         calendar.render();
-      });
-
-    </script>
-
+    });
+</script>
 
 
 
